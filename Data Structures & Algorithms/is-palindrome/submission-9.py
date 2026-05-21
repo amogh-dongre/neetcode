6bd0e9d6fd7f336ed7bfcfe4e2d8b90dbbe07b9d
Amogh Dongre <76813462+amogh-dongre@users.noreply.github.com>
2026-05-21 12:49:01 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+         
+        st =''
+        for sts in s:
+            if sts.isalnum():
+                st += sts.lower()
+        
+        st_rev = st[::-1]
+        # print(st)
+        # print(st_rev)
+        return st == st_rev
