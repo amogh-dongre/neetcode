79c22cef7ae09b9a5c2cb4f0abc4dddf9610c405
Amogh Dongre <76813462+amogh-dongre@users.noreply.github.com>
2026-06-24 16:52:31 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p,s) for p , s in zip(position , speed)]
+        pair.sort(reverse = True)
+        stack = []
+        for p , s in pair:
+            stack.append((target - p)/s)
+            if len(stack) >= 2 and stack[-1]  <= stack[-2]:
+                stack.pop()
+
+        return len(stack)          
+
+
+
+
+
+
+"""
+group the cars based on time 
+so if two cars are going to take the same time to reach dest -> same car fleet
+we will create a key,pair list of car:speed grouped in descending order
+"""
